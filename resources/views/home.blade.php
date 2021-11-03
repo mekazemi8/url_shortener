dceb2879d2df4e6762db83891c0e0e3f3222ae8b
mekazemi8 <mekazemi8@gmail.com>
2021-11-03 14:55:22 +0330

    add fonts and make ui of make url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,30 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center" style="direction: rtl">
+        <div class="col-md-7">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center">
+                    ساخت لینک کوتاه
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <form action="/action_page.php">
+                        <div class="form-group">
+                            <label for="short_link" class="float-right">لینک کوتاه</label>
+                            <div class="input-group mb-3" style="direction: ltr">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">pzly.ir/</span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="لینک کوتاه" name="short_link">
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="form-group">
+                            <label for="main_link" class="float-right">لینک اصلی</label>
+                            <input type="text" class="form-control" placeholder="لینک کوتاه نشده را وارد کنید" id="main_link" name="main_link">
+                        </div>
+                        <button type="submit" class="btn btn-success btn-block">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
