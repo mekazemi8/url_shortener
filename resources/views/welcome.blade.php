94f70ba46fc64b57d0e39c8fb1cc459ac264997f
MohammadEbrahim Kazemi <69144388+mekazemi8@users.noreply.github.com>
2021-11-10 14:16:05 +0330

    make submit persian

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                                 <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
                             </div>
-                            <button type="submit" class="btn btn-success btn-block">Submit</button>
+                            <button type="submit" class="btn btn-success btn-block">ساخت لینک</button>
                         </form>
                     </div>
                 </div>
@@ -71,7 +71,7 @@
                                 <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
                             </div>
-                            <button type="submit" class="btn btn-success btn-block">Submit</button>
+                            <button type="submit" class="btn btn-success btn-block">نمایش</button>
                         </form>
                     </div>
                 </div>
